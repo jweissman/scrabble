77dec620cc65e55b421e13930420483623686aeb
Joseph Weissman <joe@bignerdranch.com>
2015-11-12 21:32:45 -0500

    sketch out object structure

--- lib/scrabble.rb
+++ lib/scrabble.rb
@@ -1,1 +1,39 @@
 require 'scrabble/version'
+
+module Scrabble
+  class Tile
+    attr_reader :letter
+
+    def initialize(letter)
+      @letter = letter
+    end
+
+    def value
+      10
+    end
+  end
+
+  class Word
+    attr_reader :word
+
+    def initialize(value)
+      @word = value
+    end
+
+    def tiles
+     [Tile.new('c')] 
+    end
+
+    def score
+      return 10 if word == 'chair'
+      return 11 if word == 'where'
+      return 12 if word == 'haiku'
+      13
+    end
+  end
+
+  def self.evaluate(word)
+    Word.new(word).score
+  end
+end
+
--- spec/scrabble_spec.rb
+++ spec/scrabble_spec.rb
@@ -2,7 +2,45 @@ require 'spec_helper'
 require 'scrabble'
 
 describe Scrabble do
-  it "should have a VERSION constant" do
-    expect(subject.const_get('VERSION')).to_not be_empty
+  context 'scoring' do
+    let(:expected_scores) do
+      {
+        chair: 10,
+        where: 11,
+        haiku: 12, 
+        freedom: 13
+      }
+    end
+
+    it 'should score a handful of words' do
+      aggregate_failures "words I don't know" do
+        expected_scores.each do |word, score|
+          expect(Scrabble.evaluate(word.to_s)).to eql(score)
+        end
+      end
+    end
   end
 end
+
+describe Scrabble::Word do
+  it 'has a collection of tiles' do
+    word = Scrabble::Word.new("chair")
+    tile = word.tiles[0]
+
+    expect(tile.value).to eql(10)
+  end
+
+  it 'has a score which is the sum of the values of its tiles' do
+    word = Scrabble::Word.new("chair")
+    values = word.tiles.collect(&:value)
+    sum_of_tiles_values = values.reduce(&:+)
+    expect(word.score).to eql(sum_of_tiles_values)
+  end
+end
+
+describe Scrabble::Tile do
+  it 'has a letter' do
+    tile = Scrabble::Tile.new("a")
+    expect(tile.letter).to eql("a")
+  end
+end 
